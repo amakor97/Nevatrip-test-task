addbdab698800e00235eab93346e571495af47a3
amakor97 <amakor.andrei@gmail.com>
2022-10-21 19:50:09 +0300

    task 1_1: add more-times-btn

---
+++ task_1_1_src/components/Card/card.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const screenXl = 1200;
+const moreTimesBtns = document.querySelectorAll(".js-more-times-btn");
+
+moreTimesBtns.forEach(function(moreTimesBtn) {
+  let sheduleCont = moreTimesBtn.parentNode;
+  moreTimesBtn.addEventListener("click", function() {
+    if (!sheduleCont.style.maxHeight || sheduleCont.style.maxHeight === "32px" || sheduleCont.style.maxHeight === "28px") {
+      sheduleCont.style.maxHeight = "128px";
+    } else {
+      sheduleCont.style.maxHeight = (window.screen.width >= 1200) ? "32px" : "28px";
+    }
+  })
+})
