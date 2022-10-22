db1db21ca615a4f781d335b922e24e9cb86af830
amakor97 <amakor.andrei@gmail.com>
2022-10-22 13:41:13 +0300

    task 1_1: fix fonts and make other improvements

--- task_1_1_src/components/Card/card.js
+++ task_1_1_src/components/Card/card.js
@@ -9,7 +9,7 @@ moreTimesBtns.forEach(function(moreTimesBtn) {
     if (!sheduleCont.style.maxHeight || sheduleCont.style.maxHeight === "32px" || sheduleCont.style.maxHeight === "28px") {
       sheduleCont.style.maxHeight = "128px";
     } else {
-      sheduleCont.style.maxHeight = (window.screen.width >= 1200) ? "32px" : "28px";
+      sheduleCont.style.maxHeight = (window.screen.width >= screenXl) ? "32px" : "28px";
     }
   })
 })
