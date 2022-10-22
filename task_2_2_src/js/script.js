89ee813c767769e31236b59e145c381ea7dca7ab
amakor97 <amakor.andrei@gmail.com>
2022-10-22 22:23:04 +0300

    task 2_2: minor imrpovements

--- task_2_2_src/js/script.js
+++ task_2_2_src/js/script.js
@@ -7,13 +7,16 @@ const timeSelect2 = document.querySelector(".js-time-select-2");
 routeSelect.addEventListener("change", function() {
   console.log("route:", routeSelect.value);
 
+  timeSelect1.style.visibility = "visible";
+
   switch(routeSelect.value) {
     case "AtoB": {
+      timeSelect1.children[0].selected = "true";
+      timeSelect2.children[0].selected = "true";
+
       displayAllChildren(timeSelect1);
       let time1Options = [...timeSelect1.children];
-      console.log(time1Options);
       time1Options.forEach(function(opt) {
-        console.log(opt);
         if (opt.value.indexOf("BtoA") !== -1) {
           opt.style.display = "none";
         }
@@ -21,7 +24,11 @@ routeSelect.addEventListener("change", function() {
       timeSelect2.style.visibility = "hidden";
       break;
     }
+
     case "BtoA": {
+      timeSelect1.children[0].selected = "true";
+      timeSelect2.children[0].selected = "true";
+
       displayAllChildren(timeSelect1);
       let time1Options = [...timeSelect1.children];
       console.log(time1Options);
@@ -34,8 +41,19 @@ routeSelect.addEventListener("change", function() {
       timeSelect2.style.visibility = "hidden";
       break;
     }
+    
     case "Roundtrip": {
       timeSelect2.style.visibility = "visible";
+
+      displayAllChildren(timeSelect1);
+      let time1Options = [...timeSelect1.children];
+      time1Options.forEach(function(opt) {
+        if (opt.value.indexOf("BtoA") !== -1) {
+          opt.style.display = "none";
+        }
+      })
+
+      timeSelect1.children[0].selected = "true";
       break;
     }
   }
@@ -44,7 +62,10 @@ routeSelect.addEventListener("change", function() {
 
 function displayAllChildren(elem) {
   const children = [...elem.children];
-  children.forEach(function(child) {
-    child.style.display = "block";
+  children.forEach(function(child, index) {
+    if (index !== 0) {
+      child.style.display = "block";
+    }
+
   })
 }
